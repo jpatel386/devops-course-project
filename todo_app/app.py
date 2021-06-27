1f481256d1414f0299a3dbf7e7cfd889f0e502e6
Jaisal Patel <jaisal.patel@hotmail.com>
2021-06-27 23:15:29 +0100

    Add methods to display/add items and navigate to addItem page to do so

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,8 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect, url_for
 from todo_app.flask_config import Config
+
+
+from .data import session_items as session
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -8,8 +10,26 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    #return 'Hello World!'
+    print(session.get_items())
+    return render_template('index.html', items = session.get_items())
 
+
+@app.route('/addItem', methods=['GET', 'POST'])
+def add_item():
+    if request.method == 'POST':
+        item = request.form.get('item') 
+        print(item)
+        return item_added(item)
+    else:
+        return show_add_item_form()
+
+def show_add_item_form():
+        return render_template('itemForm.html')
+
+def item_added(item):
+    session.add_item(item)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
