7258786e8d0ae9d96530b872c6747b6f76dca969
Jaisal Patel <jaisal.patel@hotmail.com>
2021-07-24 10:52:34 +0100

    Changes post PR comments for exercise 1

--- todo_app/app.py
+++ todo_app/app.py
@@ -10,24 +10,16 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    #return 'Hello World!'
-    print(session.get_items())
     return render_template('index.html', items = session.get_items())
 
 
-@app.route('/addItem', methods=['GET', 'POST'])
+@app.route('/addItem', methods=['GET'])
 def add_item():
-    if request.method == 'POST':
-        item = request.form.get('item') 
-        print(item)
-        return item_added(item)
-    else:
-        return show_add_item_form()
+    return render_template('itemForm.html')
 
-def show_add_item_form():
-        return render_template('itemForm.html')
-
-def item_added(item):
+@app.route('/addItem', methods=['POST'])
+def add_item():
+    item = request.form.get('item') 
     session.add_item(item)
     return redirect(url_for('index'))
 
