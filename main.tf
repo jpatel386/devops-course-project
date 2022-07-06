56ae2cb2fb911ce83a1de25417710e6faa1100b5
Jaisal Patel <jaisal.patel@hotmail.com>
2022-07-06 22:24:24 +0100

    changes to tf

--- main.tf
+++ main.tf
@@ -16,11 +16,6 @@ provider "azurerm" {
   features {
 
   }
-  subscription_id   = var.ARM_SUBSCRIPTION_ID
-  tenant_id         = var.ARM_TENANT_ID
-  client_id         = var.ARM_CLIENT_ID
-  client_secret     = var.ARM_CLIENT_SECRET
-
 }
 
 data "azurerm_resource_group" "main" {
--- variables.tf
+++ variables.tf
@@ -6,19 +6,3 @@ variable "git_client_secret" {
   description = "The git client secret"
   sensitive = true
 }
-variable "ARM_SUBSCRIPTION_ID" {
-  description = "The ARM subscription ID"
-  sensitive = true
-}
-variable "ARM_CLIENT_SECRET" {
-  description = "The ARM client secret"
-  sensitive = true
-}
-variable "ARM_TENANT_ID" {
-  description = "The ARM tenant ID"
-  sensitive = true
-}
-variable "ARM_CLIENT_ID" {
-  description = "The ARM client ID"
-  sensitive = true
-}
